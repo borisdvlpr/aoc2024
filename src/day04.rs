0219421755b137e879c10239adb6fdef849c5f2a
borisdvlpr <eduffmiranda@hotmail.com>
2024-12-06 13:59:27 +0100

    day 4 challenge

---
+++ src/day04.rs
@@ -0,0 +1,75 @@
+use crate::helpers::read_lines;
+
+pub fn run() {
+    if let Ok(lines) = read_lines("./files/day04/data.txt") {
+        let lines: Vec<String> = lines.flatten().collect();
+        let grid: Vec<Vec<char>> = lines
+            .into_iter()
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let rows = grid.len();
+        let cols = grid[0].len();
+        let mut count = 0;
+
+        for i in 1..rows - 1 {
+            for j in 1..cols - 1 {
+                if grid[i][j] != 'A' {
+                    continue;
+                }
+
+                // m on the top
+                if i + 1 < rows
+                    && j > 0
+                    && j + 1 < cols
+                    && grid[i - 1][j - 1] == 'M'
+                    && grid[i - 1][j + 1] == 'M'
+                    && grid[i + 1][j - 1] == 'S'
+                    && grid[i + 1][j + 1] == 'S'
+                {
+                    count += 1;
+                }
+
+                // m on the bottom
+                if i + 1 < rows
+                    && j > 0
+                    && j + 1 < cols
+                    && grid[i - 1][j - 1] == 'S'
+                    && grid[i - 1][j + 1] == 'S'
+                    && grid[i + 1][j - 1] == 'M'
+                    && grid[i + 1][j + 1] == 'M'
+                {
+                    count += 1;
+                }
+
+                // m on the left
+                if i > 0
+                    && i + 1 < rows
+                    && j + 1 < cols
+                    && grid[i - 1][j - 1] == 'M'
+                    && grid[i + 1][j - 1] == 'M'
+                    && grid[i - 1][j + 1] == 'S'
+                    && grid[i + 1][j + 1] == 'S'
+                {
+                    count += 1;
+                }
+
+                // m on the right
+                if i > 0
+                    && i + 1 < rows
+                    && j > 0
+                    && grid[i - 1][j + 1] == 'M'
+                    && grid[i + 1][j + 1] == 'M'
+                    && grid[i - 1][j - 1] == 'S'
+                    && grid[i + 1][j - 1] == 'S'
+                {
+                    count += 1;
+                }
+            }
+        }
+
+        println!("'MAS' occurencies: {}", count);
+    } else {
+        println!("error: unable to read data file.");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use aoc2024::day03;
+use aoc2024::day04;
 
 pub fn main() {
     // day01::run();
     // day02::run();
-    day03::run();
+    // day03::run();
+    day04::run();
 }
